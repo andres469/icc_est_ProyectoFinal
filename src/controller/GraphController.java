d1b1afd28e51a7ca9f189188335b0d6e56149729
Erika-colla <collaguazobelen8@gmail.com>
2026-01-31 16:47:37 -0500

    Primer commit Erika

--- src/controller/GraphController.java
+++ src/controller/GraphController.java
@@ -1,5 +1,43 @@
 package controller;
 
+import java.util.ArrayList;
+import java.util.List;
+import model.*;
+import util.MetricsLogger;
+
 public class GraphController {
-    
+
+    private Graph graph;
+
+    public GraphController(Graph graph) {
+        this.graph = graph;
+    }
+
+    // 🔥 BFS / DFS con métricas completas
+    public SearchResult buscarRuta(String metodo, Node inicio, Node fin) {
+
+        List<Node> visitedNodes = new ArrayList<>();
+        List<VisitedEdge> visitedEdges = new ArrayList<>();
+        List<Node> ruta;
+
+        long startTime = System.nanoTime();
+
+        if (metodo.equalsIgnoreCase("BFS")) {
+            ruta = graph.bfs(inicio, fin, visitedNodes, visitedEdges);
+        } else {
+            ruta = graph.dfs(inicio, fin, visitedNodes, visitedEdges);
+        }
+
+        long endTime = System.nanoTime();
+
+        // 🔥 MÉTRICA COMPLETA
+        MetricsLogger.log(
+            metodo,
+            inicio.getId(),
+            fin.getId(),
+            (endTime - startTime)
+        );
+
+        return new SearchResult(ruta, visitedNodes, visitedEdges);
+    }
 }
