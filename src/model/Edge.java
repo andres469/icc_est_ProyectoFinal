f711b97997f8208e5376dd2ff24f37d09bb43e1e
Andres Tenesaca <jtenesaca74@gmail.com>
2026-01-31 16:49:37 -0500

    Segundo commit Andres

--- src/model/Edge.java
+++ src/model/Edge.java
@@ -1,5 +1,14 @@
-package model;
+    package model;
 
-public class Edge {
-    
-}
+    public class Edge {
+        private Node source;
+        private Node dest;
+
+        public Edge(Node source, Node dest) {
+            this.source = source;
+            this.dest = dest;
+        }
+        // Getters
+        public Node getSource() { return source; }
+        public Node getDest() { return dest; }
+    }
