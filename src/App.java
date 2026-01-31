de57f9623315caea3d2b3d4665e51b9a428d81cb
Andres Tenesaca <jtenesaca74@gmail.com>
2026-01-31 17:26:09 -0500

    sexto commit Andres

--- src/App.java
+++ src/App.java
@@ -1,5 +1,14 @@
+import javax.swing.SwingUtilities;
+import view.MainFrame;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        // Creamos la carpeta data si no existe para evitar errores de archivo
+        new java.io.File("data").mkdirs();
+        
+        SwingUtilities.invokeLater(() -> {
+            MainFrame frame = new MainFrame();
+            frame.setVisible(true);
+        });
     }
 }
