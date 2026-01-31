e42b8a61d1413edd08f8d6c48be904215299bac5
Erika-colla <collaguazobelen8@gmail.com>
2026-01-31 17:18:16 -0500

    tercer  commit ERIKA

--- src/model/SearchResult.java
+++ src/model/SearchResult.java
@@ -1,5 +1,30 @@
 package model;
 
-public class SearchResult {
-    
+import java.util.List;
+
+public class SearchResult{
+
+    private List<Node> path;
+    private List<Node> visitedNodes;
+    private List<VisitedEdge> visitedEdges;
+
+    public SearchResult(List<Node> path,
+                        List<Node> visitedNodes,
+                        List<VisitedEdge> visitedEdges) {
+        this.path = path;
+        this.visitedNodes = visitedNodes;
+        this.visitedEdges = visitedEdges;
+    }
+
+    public List<Node> getPath() {
+        return path;
+    }
+
+    public List<Node> getVisitedNodes() {
+        return visitedNodes;
+    }
+
+    public List<VisitedEdge> getVisitedEdges() {
+        return visitedEdges;
+    }
 }
