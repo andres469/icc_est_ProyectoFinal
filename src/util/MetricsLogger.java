c799cdf779bb7ad6e4f923a3817a03bb77512631
Erika-colla <collaguazobelen8@gmail.com>
2026-01-31 17:21:06 -0500

    cuarto commit Erika

--- src/util/MetricsLogger.java
+++ src/util/MetricsLogger.java
@@ -1,5 +1,19 @@
 package util;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 public class MetricsLogger {
-    
+
+    private static final String CSV_FILE = "data/tiempos_ejecucion.csv";
+
+    // Algoritmo, Nodo Inicio, Nodo Destino, Tiempo
+    public static void log(String algorithm, String inicio, String destino, long durationNs) {
+        try (PrintWriter out = new PrintWriter(new FileWriter(CSV_FILE, true))) {
+            out.println(algorithm + "," + inicio + "," + destino + "," + durationNs);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
