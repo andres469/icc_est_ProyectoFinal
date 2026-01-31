17c4f8b8b605069228e46eb3e543e7b73ab3fbd1
Andres Tenesaca <jtenesaca74@gmail.com>
2026-01-31 17:19:26 -0500

    tercer commit Andres

--- src/model/VisitedEdge.java
+++ src/model/VisitedEdge.java
@@ -1,5 +1,14 @@
 package model;
 
 public class VisitedEdge {
-    
+    private Node from;
+    private Node to;
+
+    public VisitedEdge(Node from, Node to) {
+        this.from = from;
+        this.to = to;
+    }
+
+    public Node getFrom() { return from; }
+    public Node getTo() { return to; }
 }
